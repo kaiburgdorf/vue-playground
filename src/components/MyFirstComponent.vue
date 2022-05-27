321c7791b87c3bc157b67ebf48e1d1937df68c72
Kai Burgdorf <k@iburgdorf.de>
2022-05-27 17:50:46 +0200

    lets go

---
+++ src/components/MyFirstComponent.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+        text: '',
+        text2: '',
+    }
+  }
+}
+</script>
+
+
+<template>
+    <h1>My First Component!</h1>
+
+    <div>
+        <input
+            :value="text"
+            @input="event => text = event.target.value">
+
+        <br>
+        <input v-model="text2">
+        <hr>
+        <p>t1: {{text}}, t2 {{text2}}</p>
+    </div>
+</template>
+
+
+
+<style>
+
+</style>
