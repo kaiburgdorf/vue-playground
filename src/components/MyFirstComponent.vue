76aac115b7f0d2261d6eed7976bd4bf7d4b39a39
Kai Burgdorf <k@iburgdorf.de>
2022-05-29 23:38:11 +0200

    add some bootstrap and axios stuff

--- src/components/MyFirstComponent.vue
+++ src/components/MyFirstComponent.vue
@@ -1,10 +1,29 @@
 <script>
+
+import axios from 'axios';
+
+
 export default {
   data() {
     return {
         text: '',
         text2: '',
+        apiReturn: '',
+        countdown: 0,
     }
+  },
+  methods: {
+      apiTest() {
+          console.log("apiTest called");
+          let returnValue = 'nothing yet';
+          //doStuff
+
+          axios
+            .get('http://localhost/helloApi.php')
+            .then(
+                response => (this.apiReturn = JSON.stringify(response.data))
+            )
+      }
   }
 }
 </script>
@@ -20,8 +39,20 @@ export default {
 
         <br>
         <input v-model="text2">
-        <hr>
         <p>t1: {{text}}, t2 {{text2}}</p>
+    </div>
+    <hr>
+    <div>
+        <button @click="apiTest()">apiTest</button>
+        <p>Return data: {{apiReturn}}</p>
+    </div>
+    <hr>
+    <div>
+        <h2>some spinning</h2>
+        <button @click="countdown=1">Start Spinning</button>
+        <button @click="countdown=0">Stop Spinning</button>
+        <p>{{countdown}}</p>
+        <b-spinner label="Spinning" v-if="countdown"></b-spinner>
     </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { BootstrapVue3 } from 'bootstrap-vue-3';
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import router from './router'
 
@@ -8,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(BootstrapVue3)
 
 app.mount('#app')
